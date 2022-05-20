3e6e3c6305afd5ba2a2f1ff4836a400b81e53728
Handrei Fontana <95292655+HandreiFontana@users.noreply.github.com>
2022-05-20 12:38:48 -0300

    feat(styles): global styles updated

--- src/styles/themes/default-theme.tsx
+++ src/styles/themes/default-theme.tsx
@@ -24,5 +24,89 @@ export const defaultTheme = createTheme({
         button: {
             textTransform: 'none'
         }
+    },
+    components: {
+        MuiCssBaseline: {
+            styleOverrides: {
+                '.MuiInputLabel-outlined.MuiInputLabel-shrink': {
+                    backgroundColor: 'white',
+                    paddingLeft: '5px',
+                    paddingRight: '5px',
+                },
+                '.MuiOutlinedInput-root': {
+                    height: '2.1em'
+                },
+                '*::-webkit-scrollbar': {
+                    width: '0.1em'
+                },
+                '*::-webkit-scrollbar-track': {
+                    WebkitBoxShadow: 'inset 0 0 6px rgba(0,0,0,0.00)'
+                },
+                '*::-webkit-scrollbar-thumb': {
+                    backgroundColor: 'rgba(0,0,0,.1)'
+                },
+                '.MuiToolbar-root.MuiToolbar-gutters': {
+                    paddingLeft: 0,
+                    marginLeft: 0,
+                },
+                '.MuiTableCell-root':
+                {
+                    borderBottom: 'none !important',
+                    padding: '0px !important',
+                    alignItems: 'center',
+                    lineHeight: '1.8rem !important'
+                },
+                '.MuiTableCell-head':
+                {
+                    lineHeight: '2.5rem !important'
+                },
+                '.MuiGrid-item': {
+                    flexGrow: 1
+                }
+            }
+        },
+
+        MuiTextField: {
+            defaultProps: {
+                variant: 'outlined',
+                size: 'small',
+                margin: 'dense',
+                autoComplete: 'off',
+                fullWidth: true
+            },
+        },
+
+        MuiInputBase: {
+            styleOverrides: {
+                input: {
+                    "&:-webkit-autofill": {
+                        transitionDelay: "9999s",
+                        transitionProperty: "background-color, color",
+                    },
+                },
+            },
+        },
+
+        MuiInputLabel: {
+            defaultProps: {
+                shrink: true
+            }
+        },
+
+        MuiButtonBase: {
+            defaultProps: {
+            }
+        }
+    },
+
+    zIndex: {
+        appBar: 1200,
+        drawer: 1100
+    },
+
+    transitions: {
+        duration: {
+            standard: 300,
+        }
     }
 })
